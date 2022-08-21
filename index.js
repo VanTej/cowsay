ac4c169e4da3a447d90e6d649ffdc3b4266c8817
VanTej <vanessatejedor@gmail.com>
2022-08-21 11:23:16 +0200

    dotenv quest

--- index.js
+++ index.js
@@ -1,15 +1,10 @@
 const cowsay = require("cowsay");
 const userInfo = require("./informations");
+require("dotenv").config();
 
 console.log(
   cowsay.say({
-    text: `I'm ${userInfo.name} from ${userInfo.campus}`,
-    f: "C3PO",
-  })
-);
-console.log(
-  cowsay.think({
-    text: "Oh non pas elle !",
-    f: "homer",
+    text: `I'm ${process.env.NAME} from ${process.env.CITY} and I speak ${process.env.LANGUAGE}`,
+    e: '^^',
   })
 );
